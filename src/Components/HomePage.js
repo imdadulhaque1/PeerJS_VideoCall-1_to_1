4bee69ddfe154d253c9a9bf7454f04f657af657e
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-12-09 13:25:30 +0600

    create remoteUser and make call user

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import Peer from 'peerjs';
+import React, {useState, useRef, useEffect} from 'react'
 
 const HomePage = () => {
+    const [peerId, setPeerId] = useState('');
+    const [remotePeerIdValue, setRemotePeerIdValue] = useState('');
+    const remoteVideoRef = useRef(null);
+    const currentUserVideoRef = useRef(null);
+    const peerInstance = useRef(null);
+
+    useEffect(() => {
+        const peer = new Peer();
+        peer.on('open', (id) => {
+            setPeerId(id);
+        })
+
+        peer.on('call', (call) =>{
+            var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+            getUserMedia({video:true, audio:true}, (mediaStream) =>{
+                currentUserVideoRef.current.srcObject = mediaStream;
+                currentUserVideoRef.current.play();
+                call.answer(mediaStream)
+                call.on('stream', function(remoteStream){
+                    remoteVideoRef.current.srcObject = remoteStream
+                    remoteVideoRef.current.play();
+                })
+            })
+        })
+
+        peerInstance.current = peer;
+    }, [])
+
+    
+
     return (
         <div>
-            <h1>Home Page!</h1>
+            <p> Current User Id Is: <b>{peerId}</b> </p>
+            <input type="text" value={remotePeerIdValue} onChange={e => setRemotePeerIdValue(e.target.value)} />
+            <button onClick={() => call(remotePeerIdValue)}>Call</button>
+            <div>
+                <video ref={currentUserVideoRef} />
+            </div>
+            <div>
+                <video ref={remoteVideoRef} />
+            </div>
         </div>
     )
 }
